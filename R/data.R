baedbd788a52de169a2b2ad7ef7756264f78be9e
DucoLam <duco.floris@gmail.cok>
2025-10-28 10:11:28 +0100

    Adds the aminoAcids objects to the package

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Amino Acids and their Basepair representation
+#'
+#' Shows all the possible basepair triplets related to all the aminoacids.
+#'
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"aminoAcids"
